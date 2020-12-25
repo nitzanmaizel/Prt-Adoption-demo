0f145442697525a1ca0545face7f574850581077
Nitzan Maizel <nitzanmaizel@gmail.com>
2020-12-25 23:52:12 +0200

    JWT web token

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 // @route    POST api/users
@@ -52,13 +54,21 @@ router.post(
 			// Save the User ==>
 			await user.save();
 			// Return jsonwebtoken ==>
-			res.send('User signup :)');
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+				if (err) {
+					throw err;
+				}
+				res.json({ token });
+			});
 		} catch (err) {
 			console.error(err.massage);
 			res.status(500).send('Server Error');
 		}
-
-		res.send('Hello World This is the Users route');
 	}
 );
 
