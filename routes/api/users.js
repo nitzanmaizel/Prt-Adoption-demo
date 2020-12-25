826b7364aa79b6855d4d5700996f5710de5d9e4c
Nitzan Maizel <nitzanmaizel@gmail.com>
2020-12-25 23:16:36 +0200

    user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
-// @route    GET api/users
-// @desc     Test route
+// @route    POST api/users
+// @desc     Sign up user
 // @access   Public
 
-router.get('/', (req, res) => {
-	res.send('Hello World This is the Users route');
-});
+router.post(
+	'/',
+	[
+		check('firstName', 'First name is required').not().isEmpty(),
+		check('lastName', 'Last name is required').not().isEmpty(),
+		check('email', 'Email is invalid').isEmail(),
+		check('password', 'Password must be greater than 6 characters').isLength({ min: 6 }),
+		check('phoneNumber', 'Phone number is invalid').not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { firstName, lastName, email, password, phoneNumber } = req.body;
+		try {
+			// See if the User exists ==>
+			// Encrypt password ==>
+			// Get users gravatar ==>
+			// Save the User ==>
+			// Return jsonwebtoken ==>
+		} catch (err) {
+			console.error(err.massage);
+			res.status(500).send('Server Error');
+		}
+
+		res.send('Hello World This is the Users route');
+	}
+);
 
 module.exports = router;
